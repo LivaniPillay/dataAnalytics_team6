0b92490f289aa4244e06849a76307bcb00fb1836
Livani Pillay <livanipillay@gmail.com>
2016-04-26 14:01:12 -0700

    writing suer data to dil

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var json2csv = require('json2csv');
+var fields = ['firstname', 'lastname', 'age', 'travellerType', 'numberOfGuests', 'isPrivateRoom', 'cleanliness', 'locationPreference', 'price'];
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
  response.sendFile(__dirname + '/views/pages/personalDetails.html');
+});
+
+app.post('/listings', function(request, response) {
+	 var obj = JSON.stringify(request.body);
+	 console.log('body: ' + obj);
+
+	 appendUserData(obj, function(err, result){
+	 	 if(result)
+	 	 {
+	 	 	console.log(result);
+	 	 	response.send(true);
+	 	 }
+	 	 else{
+		 	 console.log("Something went wrong;")	
+	 	 }
+	 });
 });
 
 app.get('/listings', function(request, response) {
@@ -25,3 +49,32 @@ app.listen(app.get('port'), function() {
 });
 
 
+fs.writeFile('userData.json', "", function(err) {
+	    if (err) throw err;
+	    console.log('file saved');
+});
+
+function appendUserData(userData, callback){
+	fs.appendFile('userData.json',"\r\n" + userData,function(err) {
+	  if (err) throw err;
+	  callback(err, true);
+	});
+}
+
+function convertToJson(){
+	 var arr =[];
+	 arr.push(request.body);
+
+	 console.log("arr " + JSON.stringify(arr));
+	 json2csv({ data: arr}, function(err, csv) {
+	  if (err) console.log(err);
+	  fs.appendFile('userData.csv',csv, function(err) {
+	    if (err) throw err;
+	    console.log('file saved');
+	  });
+	});
+}
+
+
+
+	
--- public/app.js
+++ public/app.js
@@ -4,7 +4,29 @@ $(function(){
 
 	$("#personalDetailsSubmit").on('click', function(event){
 
-		window.location = '../listings';
+		var info ={};
+		info.firstname = $("#firstname").val();
+		info.lastname = $("#lastname").val();
+		info.age = $("#age").val();
+		info.travellerType = $("input[name=travellerType]:checked").val();
+		info.numberOfGuests = $("#numberOfGuests").val();
+		info.isPrivateRoom = $('#isPrivateRoom').is(':checked');
+		info.cleanliness = $("input[name=cleanliness]:checked").val();
+		info.locationPreference = $("input[name=locationPreference]:checked").val();
+		info.price = $("input[name=price]:checked").val();
+
+		$.ajax({
+			type: 'POST',
+			data: JSON.stringify(info),
+	        contentType: 'application/json',
+            url: 'http://localhost:5000/listings',						
+            success: function(data) {
+                console.log(data);
+              	console.log(JSON.stringify(info));
+              	window.location = '../listings';
+            }});
+		
+
 	});
 
 	$("#listingSelectionSubmit").on('click', function(event){
